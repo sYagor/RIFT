d3ed5d934ac4e91f38abc647bc13c3119cd4be84
Johnatan Marinho <johnatan.marinho@gmail.com>
2017-11-08 15:19:42 -0200

    add rocket

---
+++ assets/js/rocket.js
@@ -0,0 +1,23 @@
+class Rocket {
+  constructor(position, direction) {
+    this.sprite = loadImage("assets/images/rocket.png");
+    //posicao e direcao sao as mesmas do player 
+    this.pos = position;
+    this.direction = direction;
+    this.width = 30;
+    this.height =40;
+    this.visible = false;
+  }
+
+  render(){
+    image(
+      this.sprite,
+      0,
+      0,
+      this.width,
+      this.height
+    );
+  }
+
+
+}
